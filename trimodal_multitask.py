d9fbac650b96d40b0104c6a0e0957e3e5c61b8fd
cosmonauto <cosmonautoyu@proton.me>
2022-09-21 17:26:27 +0000

    Update projects.html

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -16,4 +16,4 @@ import gc
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 from scipy.stats import pearsonr
 from scipy.spatial.distance import cosine
-#=============
+#============================================
