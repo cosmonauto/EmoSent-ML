94aa7b24a9a01f90b7c66b5f2c44f26d7f0744d4
cosmonauto <cosmonautoyu@proton.me>
2022-09-30 16:24:51 +0000

    ARM: topology: save cpu id of each core

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -20,4 +20,5 @@ from scipy.spatial.distance import cosine
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "3"
-#========================================
+#=============================================================
+import tens
