21bf28c933e299b0cd6968482ffc6079012925d6
cosmonauto <cosmonautoyu@proton.me>
2022-10-10 18:59:03 +0000

    add scrutinizer config

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -34,4 +34,5 @@ def attention(att_type, x, y):
     if att_type == 'simple':
         m_dash = dot([x, y], axes=[2,2])
         m = Activation('softmax')(m_dash)
-        h_dash = dot([m, y], ax
+        h_dash = dot([m, y], axes=[2,1])
+        return multiply([
