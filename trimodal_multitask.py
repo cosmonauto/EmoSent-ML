b1e33b0ffeee2cb298e7414ae36f968f8fc58934
cosmonauto <cosmonautoyu@proton.me>
2022-11-01 06:07:37 +0000

    version 1.6

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -59,4 +59,5 @@ def emotionClass(testLabel):
     trueLabel = np.array(trueLabel)
     return trueLabel
 
-def seventhClass(inputLabel, m
+def seventhClass(inputLabel, mask):
+    updatedLabel = np
