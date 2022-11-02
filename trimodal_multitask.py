9ef83bced05caaa43364281ae525b03d28d430b5
cosmonauto <cosmonautoyu@proton.me>
2022-11-02 08:09:07 +0000

    added desc

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -61,4 +61,5 @@ def emotionClass(testLabel):
 
 def seventhClass(inputLabel, mask):
     updatedLabel = np.zeros((inputLabel.shape[0],inputLabel.shape[1],7), dtype ='float')
-    for i in range(inputLabe
+    for i in range(inputLabel.shape[0]):
+        for j i
