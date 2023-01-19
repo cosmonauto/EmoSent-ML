284299ad11aa92497970c7084ea99d59b06407bc
cosmonauto <cosmonautoyu@proton.me>
2023-01-19 19:53:29 +0000

    Version 1.1r39

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -112,4 +112,4 @@ def featuresExtraction():
 
     max_segment_len = train_text.shape[1]
 
-    train_mask = np.zeros((train_
+    train_mask = np.zeros((train_video.shape[0], train_video.shape[1])
