24f40202b7321ff329e6e12fe56bc0f813a549fb
cosmonauto <cosmonautoyu@proton.me>
2023-05-07 15:35:53 +0000

    Updated the trimesh feedstock.

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -143,4 +143,5 @@ def calc_valid_result_emotion(result, test_label, test_mask):
 
     for i in range(result.shape[0]):
         for j in range(result.shape[1]):
-      
+            if test_mask[i,j]==1:
+            
