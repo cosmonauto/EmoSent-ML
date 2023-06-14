ccadc4e55976a77adad56128ec7eee5f63946b4d
cosmonauto <cosmonautoyu@proton.me>
2023-06-14 18:05:06 +0000

    added some screenshots

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -149,4 +149,4 @@ def calc_valid_result_emotion(result, test_label, test_mask):
     true_label      = np.array(true_label)
     predicted_label = np.array(predicted_label)
 
-    r
+    return true_label, predicted
