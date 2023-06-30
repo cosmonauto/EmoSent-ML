3b6ecd02f31195b1b8c8fd44a4f54dd37d5229c8
cosmonauto <cosmonautoyu@proton.me>
2023-06-30 06:43:49 +0000

    Update tr.po

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -159,4 +159,5 @@ def calc_valid_result_sentiment(result, test_label, test_mask):
     for i in range(result.shape[0]):
         for j in range(result.shape[1]):
             if test_mask[i,j]==1:
-                true_label.append(n
+                true_label.append(np.argmax(test_label[i,j] ))
+  
