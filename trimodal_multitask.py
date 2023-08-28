4491ba1dbb157e7f3ce8b992bb003defe06cd9f1
cosmonauto <cosmonautoyu@proton.me>
2023-08-28 14:28:56 +0000

    Update branch aliases

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -213,4 +213,4 @@ class MetricsCallback(keras.callbacks.Callback):
 
         true_label_senti, predicted_label_senti = calc_valid_result_sentiment(y_prediction[0], y_actual[0], test_mask)
         w_acc, TP, TN, FP, FN, P, N = weighted_accuracy(true_label_senti, predicted_label_senti)
-        op
+        open('results/'+modality+'_senti.txt', 'a
