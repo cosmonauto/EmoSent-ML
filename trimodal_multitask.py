b001868daf0dd112875bde4ccb36bc523a012391
cosmonauto <cosmonautoyu@proton.me>
2023-10-07 12:54:12 +0000

    Drivers: hv: vmbus: Don't wait after requesting offers

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -224,4 +224,4 @@ class MetricsCallback(keras.callbacks.Callback):
         th=[0.10,0.15,0.16,0.17,0.18,0.19,0.20,0.21,0.22,0.23,0.24,0.25,0.30,0.35,0.40,0.50]
         for t in range(len(th)):
             print th[t]
-            emotion = ['Anger', 'Disgust', 'Fear', 'Happy', 'Sad
+            emotion = ['Anger', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'No Clas
