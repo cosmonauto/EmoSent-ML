1d3d75b0e683a395301d719825ed7e4552ed685d
cosmonauto <cosmonautoyu@proton.me>
2023-11-18 22:48:18 +0000

    Update tests.yml

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -240,4 +240,4 @@ class MetricsCallback(keras.callbacks.Callback):
                 F1Score_emo.append(precision_recall_fscore_support(true_label_emo[:,i], predicted_label_emo[:,i], average='weighted')[2])
                 wAcc_emo.append(weighted_accuracy(true_label_emo[:,i], predicted_label_emo[:,i])[0])
 
- 
+                w_acc, TP, T
