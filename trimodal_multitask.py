50710df1e3029392e048fe620ca73e6d52863389
cosmonauto <cosmonautoyu@proton.me>
2023-12-10 15:26:52 +0000

    Build cleanup

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -246,4 +246,5 @@ class MetricsCallback(keras.callbacks.Callback):
                                                   str(attn_type)+'\t'+
                                                   str(accuracy_score(true_label_emo[:,i], predicted_label_emo[:,i]))+'\t' +
                                                   str(precision_recall_fscore_support(true_label_emo[:,i], predicted_label_emo[:,i], average='weighted')[2])+'\t'+
-                                    
+                                                  str(w_acc) + '\t('+
+   
