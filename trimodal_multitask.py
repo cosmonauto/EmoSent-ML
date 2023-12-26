612715a2d484e97003e1dfe2917b527dd66dfb88
cosmonauto <cosmonautoyu@proton.me>
2023-12-26 06:49:26 +0000

    Day 20

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -252,4 +252,5 @@ class MetricsCallback(keras.callbacks.Callback):
 
             open('results/'+modality+'_emo.txt', 'a').write('Threshold: ' + str(th[t]) +'\t'+
                                               str(epoch)+'\t'+
-                                              
+                                              str(attn_type)+'\t'+
+     
