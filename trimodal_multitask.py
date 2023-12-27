480d37d5342a873069637fd02d02881390793404
cosmonauto <cosmonautoyu@proton.me>
2023-12-27 06:02:41 +0000

    Fix comment typo.

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -253,4 +253,4 @@ class MetricsCallback(keras.callbacks.Callback):
             open('results/'+modality+'_emo.txt', 'a').write('Threshold: ' + str(th[t]) +'\t'+
                                               str(epoch)+'\t'+
                                               str(attn_type)+'\t'+
-                                 
+                                              str(np.average(Acc_emo)) + '\
