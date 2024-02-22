56ecb341e7dce1d0a90ca412e41a022d35a14d7c
cosmonauto <cosmonautoyu@proton.me>
2024-02-22 20:55:25 +0000

    Fix wrong method name

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -282,4 +282,4 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
         r_units = 200
 
         if recurrent:
-            rnn_text = B
+            rnn_text = Bidirectional(GRU(r_units, return_sequen
