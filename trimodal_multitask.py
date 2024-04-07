16e76aee51a73ea34cd10b0e8910d392cf33f89c
cosmonauto <cosmonautoyu@proton.me>
2024-04-07 16:43:35 +0000

    add thread

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -307,4 +307,6 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
         else:
             td_text = inter_text
             td_audio = inter_audio
-            td_video = inter_vi
+            td_video = inter_video
+
+        ########### attenti
