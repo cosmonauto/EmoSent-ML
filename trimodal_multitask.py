6bd4aec32747a36dfdc89231e0c7ddd0c11183ac
cosmonauto <cosmonautoyu@proton.me>
2024-04-27 22:38:41 +0000

    utf-8 encoding

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -321,4 +321,5 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
 
             merged = concatenate([va_att, vt_att, av_att, at_att, tv_att, ta_att, td_video, td_audio, td_text])
 
-        ## uni modal cross utterance atte
+        ## uni modal cross utterance attention ##
+        elif attn_typ
