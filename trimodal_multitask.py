59025db07f0d167ce56411a4aabe5865b27e5163
cosmonauto <cosmonautoyu@proton.me>
2024-05-25 16:57:07 +0000

    vfs,proc: guarantee unique inodes in /proc

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -337,4 +337,4 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
 
         ########### output layer ############
         output_sentiment = TimeDistributed(Dense(2, activation='softmax'), name='output_sentiment')(merged)
-        output_emotion   = TimeDistributed(Dense(7, activation='s
+        output_emotion   = TimeDistributed(Dense(7, activation='sigmoid'), name='output_emotion')(me
