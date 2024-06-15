c9cb6fef636bf9bfb0915b4c6f118fa35913806b
cosmonauto <cosmonautoyu@proton.me>
2024-06-15 19:27:38 +0000

    Updated copyright years

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -344,4 +344,4 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
         ###################### model training #######################
         np.random.seed(run)
         path1 = 'weights/sentiment_'+modality+'_'+str(run)+'.hdf5'
-        path2
+        path2 = 'weights/emotion_'
