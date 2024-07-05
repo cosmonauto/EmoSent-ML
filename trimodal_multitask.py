6181aef0a9eeca05d78d307d14bed474d8ca6c4d
cosmonauto <cosmonautoyu@proton.me>
2024-07-05 19:31:53 +0000

    Update TODO.

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -351,4 +351,4 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
         bestModel_sentiment = ModelCheckpoint(path1, monitor='val_output_sentiment_acc', verbose=1, save_best_only=True, mode='max')
         bestModel_emotion = ModelCheckpoint(path2, monitor='val_output_emotion_acc', verbose=1, save_best_only=True, mode='max')
 
-       
+        metrics_callback = M
