fc35f50bd478215ac6e3217f067723d6d4e920cb
cosmonauto <cosmonautoyu@proton.me>
2024-07-17 17:11:40 +0000

    better parsing

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -353,4 +353,5 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
 
         metrics_callback = MetricsCallback(test_data=([test_text, test_audio, test_video], [senti_test_label, emo_test_label]))
         history = model.fit([train_text, train_audio, train_video], [senti_train_label, emo_train_label],
-                            epochs=
+                            epochs=50,
+                      
