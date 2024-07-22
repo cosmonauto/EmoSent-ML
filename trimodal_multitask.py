4c3c60db223f24ccaefcca61238ad37135986a98
cosmonauto <cosmonautoyu@proton.me>
2024-07-22 19:42:46 +0000

    merge conflict fixed

--- trimodal_multitask.py
+++ trimodal_multitask.py
@@ -357,4 +357,5 @@ def multimodal_cross_attention(attn_type, recurrent, timedistributed):
                             batch_size=16,
                             sample_weight=[train_mask, train_mask],
                             shuffle=True,
-                            callbacks=[metrics_callback, bestModel_sentiment, bestModel
+                            callbacks=[metrics_callback, bestModel_sentiment, bestModel_emotion],
+          
